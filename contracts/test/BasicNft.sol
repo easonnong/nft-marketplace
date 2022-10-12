2343557c1fad210c23768992774646324eb16446
easonnong <nongeason@gmail.com>
2022-10-12 21:01:42 +0800

    basic nft

---
+++ contracts/test/BasicNft.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+  string public constant TOKEN_URI =
+    "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+  uint256 private s_tokenCounter;
+
+  event DogMinted(uint256 indexed tokenId);
+
+  constructor() ERC721("Dogie", "DOG") {
+    _safeMint(msg.sender, s_tokenCounter);
+    s_tokenCounter++;
+  }
+
+  function mintNft() public {
+    _safeMint(msg.sender, s_tokenCounter);
+    emit DogMinted(s_tokenCounter);
+    s_tokenCounter++;
+  }
+
+  function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    return TOKEN_URI;
+  }
+
+  function getTokenCounter() public view returns (uint256) {
+    return s_tokenCounter;
+  }
+}
