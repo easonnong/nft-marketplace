707b196cc96acd1aa0b51ec706d694338b9dc536
easonnong <nongeason@gmail.com>
2022-10-16 10:03:53 +0800

    approvals unit test

--- test/unit/NftMarketplace_unit_test.js
+++ test/unit/NftMarketplace_unit_test.js
@@ -1,6 +1,7 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
 const { assert, expect } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
+const { CustomError } = require("hardhat/internal/hardhat-network/stack-traces/model")
 const { developmentChains } = require("../../helper-hardhat-config")
 
 !developmentChains.includes(network.name)
@@ -21,14 +22,36 @@ const { developmentChains } = require("../../helper-hardhat-config")
         basicNft = basicNftContract.connect(deployer)
         await basicNft.mintNft()
         await basicNft.approve(nftMarketplaceContract.address, TOKEN_ID)
+        return { user }
       }
 
       describe("listItem", function () {
+        it("exclusively items that haven't been listed", async () => {
+          await loadFixture(deployContractLockFixture)
+          await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+          await expect(
+            nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+          ).to.be.revertedWithCustomError(nftMarketplace, "AlreadyListed")
+        })
+
+        it("exclusively allows owners to list", async () => {
+          const { user } = await loadFixture(deployContractLockFixture)
+          nftMarketplace = await nftMarketplaceContract.connect(user)
+          await basicNft.approve(user.address, TOKEN_ID)
+          await expect(
+            nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+          ).to.be.revertedWithCustomError(nftMarketplace, "NotOwner")
+        })
+        it.only("need approvals to list item", async () => {
+          await loadFixture(deployContractLockFixture)
+          await basicNft.approve(ethers.constants.AddressZero, TOKEN_ID)
+          await expect(
+            nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+          ).to.be.revertedWithCustomError(nftMarketplace, "NotApprovedForMarketplace")
+        })
         it("emits an event after listing an item", async () => {
           await loadFixture(deployContractLockFixture)
-          console.log(`nftMarketplace address:${nftMarketplace.address}`)
-          console.log(`nftMarketplaceContract:${nftMarketplaceContract.address}`)
-          await expect(nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)).to.be.emit(
+          await expect(nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)).to.emit(
             nftMarketplace,
             "ItemListed"
           )
